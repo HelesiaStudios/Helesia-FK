212544de8986e928a3e134d8f90bc284a96475e3
6myy <155362207+6myy@users.noreply.github.com>
2024-01-27 17:52:29 +0100

    update

--- src/main/java/fr/helesia/fallenkingdoms/listeners/player/PlayerJoinListener.java
+++ src/main/java/fr/helesia/fallenkingdoms/listeners/player/PlayerJoinListener.java
@@ -46,7 +46,7 @@ public class PlayerJoinListener implements Listener {
 			new LobbyRunnable().runTaskTimer(Main.getINSTANCE(), 0L, 20L);
 			Main.getINSTANCE().lobbyRunnable.start = true;
 		}
-		ItemStack selectTeam = new ItemBuilder(Material.c).setName("§6§lChoisir une équipe §7(Clic droit)").toItemStack();
+		ItemStack selectTeam = new ItemBuilder(Material.STAINED_CLAY).setName("§6§lChoisir une équipe §7(Clic droit)").toItemStack();
 		player.getInventory().setItem(0, selectTeam);
 	}
 
