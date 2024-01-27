3197191f5d307d8de05ba3955096cd7ffe4b403f
6myy <155362207+6myy@users.noreply.github.com>
2024-01-27 17:23:34 +0100

    Fix bug

--- src/main/java/fr/helesia/fallenkingdoms/listeners/player/PlayerJoinListener.java
+++ src/main/java/fr/helesia/fallenkingdoms/listeners/player/PlayerJoinListener.java
@@ -3,10 +3,10 @@ package fr.helesia.fallenkingdoms.listeners.player;
 import fr.helesia.fallenkingdoms.GameStatus;
 import fr.helesia.fallenkingdoms.Main;
 import fr.helesia.fallenkingdoms.player.GamePlayer;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
-import org.bukkit.Material;
+import fr.helesia.fallenkingdoms.utils.DyeColor;
+import fr.helesia.fallenkingdoms.utils.ItemBuilder;
+import jdk.internal.org.jline.utils.Colors;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -23,12 +23,17 @@ public class PlayerJoinListener implements Listener {
 		
 		if (!GameStatus.isStatus(GameStatus.ATTENTE)) {
 			player.setGameMode(GameMode.SPECTATOR);
+			Location spawn = new Location(Bukkit.getWorld("world"), 2000.439, 66.0, 2000.512);
+			player.teleport(spawn);
 			player.sendMessage(ChatColor.GOLD + "La partie a déjà commencé vous êtes désormais spectateur.");
 			
 			new GamePlayer(player.getName());
 			return;
 		}
-		
+
+		Location spawn = new Location(Bukkit.getWorld("world"), 2000.439, 66.0, 2000.512);
+		player.teleport(spawn);
+
 		event.setJoinMessage(Main.getINSTANCE().getPrefix() + "§e" + player.getName() + " §7a rejoint la partie §a(" + Bukkit.getOnlinePlayers().size() + "§a/" + Bukkit.getMaxPlayers() + ")");
 		
 		new GamePlayer(player.getName());
@@ -39,8 +44,7 @@ public class PlayerJoinListener implements Listener {
 			new LobbyRunnable().runTaskTimer(Main.getINSTANCE(), 0L, 20L);
 			Main.getINSTANCE().lobbyRunnable.start = true;
 		}
-		
-		// item du menu des teams.
-		player.getInventory().setItem(4, new ItemStack(Material.NETHER_STAR, 1));
+		ItemStack selectTeam = new ItemBuilder(Material.STAINED_CLAY).setDyeColor(DyeColor.WHITE).setName("§6§lChoisir une équipe §7(Clic droit)").toItemStack();
+		player.getInventory().setItem(0, selectTeam);
 	}
 }
