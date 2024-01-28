08c72887bd92f964b6dd6b4d178cf587ed7e5087
6myy <155362207+6myy@users.noreply.github.com>
2024-01-28 17:44:28 +0100

    FIIIIIIIIIIIIIIIIIIIIIIIIIIX

--- src/main/java/fr/helesia/fallenkingdoms/listeners/ListenersManager.java
+++ src/main/java/fr/helesia/fallenkingdoms/listeners/ListenersManager.java
@@ -28,6 +28,7 @@ public class ListenersManager {
 		pm.registerEvents(new PlayerQuitListener(), this.plugin);
 		pm.registerEvents(new EntityDamageListener(), this.plugin);
 		pm.registerEvents(new PlayerDeathEvent(), this.plugin);
+		pm.registerEvents(new PlayerWaiting(), this.plugin);
 		
 		pm.registerEvents(new TeamsMenu(), this.plugin);
 	}
