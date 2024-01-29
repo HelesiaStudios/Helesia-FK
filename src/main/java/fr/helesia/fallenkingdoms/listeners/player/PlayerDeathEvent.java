dac5fff199635a86920d662e152249ce4144f733
6myy <155362207+6myy@users.noreply.github.com>
2024-01-29 14:10:52 +0100

    Fix scoreboard timer and pvp

--- src/main/java/fr/helesia/fallenkingdoms/listeners/player/PlayerDeathEvent.java
+++ src/main/java/fr/helesia/fallenkingdoms/listeners/player/PlayerDeathEvent.java
@@ -8,6 +8,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 public class PlayerDeathEvent implements Listener {
+    private Location redSpawn, blueSpawn;
+
+    public PlayerDeathEvent() {
+        this.redSpawn = new Location(Bukkit.getWorld("world"), 1999.628, 65.56250, 2108.371);
+        this.blueSpawn = new Location(Bukkit.getWorld("world"), 2001.685, 65.56250, 1892.300);
+    }
 
     @EventHandler
     public void Death(org.bukkit.event.entity.PlayerDeathEvent e){
@@ -18,14 +24,33 @@ public class PlayerDeathEvent implements Listener {
         victim.setHealth(victim.getMaxHealth());
         victim.resetMaxHealth();
         victim.setGameMode(GameMode.SPECTATOR);
+        Location lastDeath = new Location(victim.getWorld(), victim.getLocation().getX(), victim.getLocation().getY(), victim.getLocation().getZ());
 
         for(Player players : Bukkit.getOnlinePlayers()){
+            if(Main.getINSTANCE().pvp == false){
+                victim.teleport(lastDeath);
+                victim.sendMessage("§cLe pvp n'est pas encore actif !");
+            }
+
             if(Bukkit.getOnlinePlayers().contains(attacker)){
                 Bukkit.broadcastMessage("§7✝ §f" + Main.getINSTANCE().getTeam(victim) + " " + victim.getName() + " §7a été tué(e) par §f" + Main.getINSTANCE().getTeam(attacker) + " " + attacker.getName());
+                if (Main.getINSTANCE().red_team.contains(victim.getUniqueId())) {
+                    victim.teleport(getRedSpawn());
+                } else if (Main.getINSTANCE().blue_team.contains(victim.getUniqueId())) {
+                    victim.teleport(getBlueSpawn());
+                }
             }
 
             Bukkit.broadcastMessage("§7✝ §f" + Main.getINSTANCE().getTeam(victim) + " " + victim.getName() + " §7a été tué(e) par §b" + attacker);
         }
 
+    }
+
+    public Location getBlueSpawn() {
+        return blueSpawn;
+    }
+
+    public Location getRedSpawn() {
+        return redSpawn;
     }
 }
